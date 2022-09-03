2e748970ab0c2295380af010aa8f15ca1812ba88
stay7 <codelatte20@gmail.com>
2022-09-03 16:05:58 +0900

    로그인 페이지 만듬

---
+++ lib/controller/login_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  static const String google = "Google";
+  static const String kakao = "Kakao";
+  static const String facebook = "Facebook";
+  static const String naver = "Naver";
+
+  void loginGoogle() {
+
+  }
+}
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/route_manager.dart';
+import 'package:happiness_client/controller/login_controller.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  final String title = '우리 이제\n아껴서\n잘 살아봐요';
+  final String name = 'Naming';
+
+  @override
+  Widget build(BuildContext context) {
+    final LoginController c = Get.put(LoginController());
+
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(title),
+            Text(name),
+            Row(
+              children: [LoginButton()],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final LoginController c = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: c.loginGoogle, child: const Text(LoginController.naver));
+  }
+}
