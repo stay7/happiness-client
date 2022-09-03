5013e09cabd6a029f42788dc826ef823a0d153d5
stay7 <codelatte20@gmail.com>
2022-09-03 16:55:32 +0900

    setting google_sign_in for ios

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,4 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/*
+firebase documentation : https://firebase.google.com/docs/auth/flutter/federated-auth
+ */
 
 class LoginController extends GetxController {
   static const String google = "Google";
@@ -6,7 +14,15 @@ class LoginController extends GetxController {
   static const String facebook = "Facebook";
   static const String naver = "Naver";
 
-  void loginGoogle() {
+  final GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email', 'profile']);
 
+  Future<UserCredential> loginGoogle() async {
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential =
+        GoogleAuthProvider.credential(accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+
+    debugPrint("$googleUser, $googleAuth, $credential");
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
